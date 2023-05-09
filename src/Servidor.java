d72f009e50c2175e0db005658bc8c320c983037b
cah81 <carlosenriqueanguloherrera@gmail.com>
2023-05-09 18:47:43 +0200

    se agrega mejora en el mensaje personalizado en el lado del server

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,9 +1,7 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 import java.util.Scanner;
+
 
 public class Servidor {
 
